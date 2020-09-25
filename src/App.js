7837c498f33eebc9e153181fd58fc8448f21b379
Rabiul Alam <rabiul.alam@outlook.com>
2020-09-25 00:04:56 -0400

    Skyscanner Function, need to adjust the date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /*global google*/
 import React, { useState, useEffect } from "react";
 import { withScriptjs } from "react-google-maps";
+import axios from "axios";
 import "./App.css";
 
 //importing Componenets
@@ -8,7 +9,9 @@ import ProgrammingQuote from "./Components/Programming-quote";
 import Map from "./Components/Map";
 import PlacesAutocomplete from "./Components/PlacesAutoComplete";
 import RouteDetail from "./Components/RouteDetails";
-import SkyScanner from "./Components/SkyScanner";
+
+//importing local JSON
+import airports from "./airports.json";
 
 function App() {
   const [dir, setDir] = useState({});
@@ -17,6 +20,10 @@ function App() {
     lat: 40.756795,
     lng: -73.954298,
   });
+  const [fromAirport, setFromAirport] = useState("MCO");
+  const [toAirport, setToAirport] = useState("JFK");
+  const [flightDetail, setFlightDetail] = useState({});
+
   //User Location
   function getLocation() {
     if (navigator.geolocation) {
@@ -42,8 +49,22 @@ function App() {
       },
       (result, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
-          //console.log(result);
           setDir(result);
+          let originState = result.routes?.[0].legs[0].start_address
+            .split(", ")
+            .slice(-2)[0]
+            .split(" ")[0]
+            .toString();
+          let destinationState = result.routes?.[0].legs[0].end_address
+            .split(", ")
+            .slice(-2)[0]
+            .split(" ")[0]
+            .toString();
+
+          let destinationAirport = airports?.[0][destinationState];
+          let orginAirport = airports?.[0][originState];
+          setFromAirport(orginAirport);
+          setToAirport(destinationAirport);
         } else {
           console.error(`error fetching directions ${result}`);
         }
@@ -51,8 +72,34 @@ function App() {
     );
   };
 
+  // *******************************SkyScanner*****************************************
+  function updateFlightInfo() {
+    axios({
+      method: "GET",
+      url: `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${fromAirport}-sky/${toAirport}-sky/2020-09-28`,
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host":
+          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        "x-rapidapi-key": `${process.env.REACT_APP_RAPIDAPI_KEY}`,
+        useQueryString: true,
+      },
+      params: {
+        inboundpartialdate: "2020-09-30",
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setFlightDetail(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   useEffect(() => {
     updateDirections();
+    updateFlightInfo();
   }, [destination]);
   //Google Map Loader
   const MapLoader = withScriptjs(() => Map({ dir }));
@@ -70,8 +117,6 @@ function App() {
       />
 
       <RouteDetail dir={dir} />
-
-      <SkyScanner dir={dir} />
     </div>
   );
 }
